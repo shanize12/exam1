3209fbebd414f5afd1d3b9edbfcb37540744a748
exam <exam@Bcalab4-m06>
2023-05-04 12:17:23 +0530

    First commit

---
+++ CaseToolsExam/src/Square_230.java
@@ -0,0 +1,13 @@
+
+public class Square_230 {
+ int length;
+ Square_230(int length)
+ {
+	 this.length=length;
+ }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
